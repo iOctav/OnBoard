217cff5454510c90ec08942414b9f76c0a33329b
Andrey Cherevichnik <ioctav@icloud.com>
2023-03-24 01:20:50 +0000

    add a field selector for nested swimlanes

--- src/components/NestedSwimlanesSettings/index.js
+++ src/components/NestedSwimlanesSettings/index.js
@@ -3,16 +3,17 @@ import Button from '@jetbrains/ring-ui/dist/button/button';
 import { useTranslation } from 'react-i18next';
 import NestedSwimlanesList from './NestedSwimlanesList';
 
-function NestedSwimlanesSettings({swimlaneSettings, switchToColAndSwimTab}) {
+function NestedSwimlanesSettings({swimlaneSettings, switchToColAndSwimTab, projectShortNames}) {
   const { t } = useTranslation();
   if (!swimlaneSettings.enabled)
     return (<span>{t('Swimlanes are disabled. To turn on then go to tab')}<Button text onClick={switchToColAndSwimTab}>"{t('Columns and Swimlanes')}"</Button></span>);
-  return (<NestedSwimlanesList/>);
+  return (<NestedSwimlanesList projectShortNames={projectShortNames}/>);
 }
 
 NestedSwimlanesSettings.propTypes = {
   swimlaneSettings: PropTypes.object,
   switchToColAndSwimTab: PropTypes.func.isRequired,
+  projectShortNames: PropTypes.arrayOf(PropTypes.string).isRequired,
 }
 
 export default NestedSwimlanesSettings;
